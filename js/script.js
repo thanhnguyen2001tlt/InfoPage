c8d532704b8bf8c2c4c3b00fe1f6f12f9558803d
Thanh Nguyen <80317178+thanhnguyen2001tlt@users.noreply.github.com>
2023-01-30 13:18:14 +0700

    update

--- js/script.js
+++ js/script.js
@@ -111,9 +111,9 @@ document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 document.onkeydown = function (e) {
   // disable F12 key
-  if (e.keyCode == 123) {
-    return false;
-  }
+  // if (e.keyCode == 123) {
+  //   return false;
+  // }
 
   // disable I key
   if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
